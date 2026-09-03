0b57728e5258f6ec6f6c73a2e556c68ce533dc44
tenerifewonders <tenerifewonders@gmail.com>
2026-09-03 11:06:33 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "santa-cruz-fr-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./FR-SANTA-CRUZ.html",
+  "./manifest.json",
+  "./santa-cruz.geojson",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
